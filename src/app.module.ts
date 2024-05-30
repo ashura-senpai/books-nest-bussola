100d4515955643fbaa038936bf97141393c6b3bb
ashuraa <91293924+ashura-senpai@users.noreply.github.com>
2024-05-30 19:29:18 -0300

    last commits

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { BooksModule } from './books/books.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ProductModule } from './product/product.module';
+import { MultiplicateProductPriceMiddleware } from './product/middlewares/multiplicate-product-price.middleware';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://0.0.0.0/books-nest'),
     BooksModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/'),
+    ProductModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(MultiplicateProductPriceMiddleware).forRoutes('product');
+  }
+}
